1032ea764e78e4fcaf65844765f0ed4a08ea3344
Heather Angel Chandler <heatherimac@Heathers-iMac.local>
2018-10-03 10:42:07 -0500

    adding view pages and log out button from firebase

---
+++ src/views/Events.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Button, Text } from 'native-base'
+export default class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Events</Text>
+      </View>
+    )
+  }
+}
